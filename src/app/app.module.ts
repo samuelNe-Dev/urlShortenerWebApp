724da4aa0ff085ede2d5ad687debd6a7eb1b4125
Samuel Negash <negash.samuel@yahoo.de>
2022-03-06 15:30:26 +0100

    Added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { InfoCardComponent } from './statistics/info-card/info-card.component';
+import { CtaSectionComponent } from './cta-section/cta-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    HeroComponent,
+    StatisticsComponent,
+    InfoCardComponent,
+    CtaSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
